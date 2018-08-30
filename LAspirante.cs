f4ae5ea7ff1173b3526bd395270bf559c2aa9f21
CrostRomm <cyesidromero@ucundinamarca.edu.co>
2018-08-30 18:03:31 -0500

    Log in, recuperar y datos personales aspirante
    Se me toteo el git hub así que lo subo desde el explorador

---
+++ LAspirante.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+using Data;
+
+namespace Logica
+{
+    public class LAspirante
+    {
+        public UAspirante ValidacionUrl(UAspirante link)
+        {
+            if (link.IdUser.Equals(null) ||  link.UserName.Equals(null) || link.RolId.Equals(null) || link.RolId != 2)
+            {
+                link.IdUser.Equals(null);
+                link.UserName.Equals(null);
+                link.RolId.Equals(null);
+                link.Url = @"<script type='text/javascript'>Redir_Login();</script>";
+                
+            }
+
+            return link;
+        }
+        public UAspirante ValidacionHora(UAspirante hora)
+        {
+            UAspirante ValFecha = new UAspirante();
+            if (hora.FechaNacimiento > hora.ControlTime)
+            {
+                ValFecha.Mensaje = "No puede seleeccionar una fecha anterior";
+            }
+            else
+            {
+                DAspirante llenaDatosP = new DAspirante();
+                llenaDatosP.RegistraAspirante(hora);
+                ValFecha.Url2= @"<script type='text/javascript'>Redir_FormacionA();</script>";
+                ValFecha.Url = "FormacionA.aspx";
+
+            }
+            return ValFecha;
+        }
+    }
+}
---
+++ js/Redirecciones.js
@@ -0,0 +1,27 @@
+﻿
+function Redir_InicioEmpresa()
+{
+
+        window.location.href = "VerAspirantes.aspx";
+}
+function Redir_InicioAspirante()
+{
+
+    window.location.href = "VerOfertas.aspx";
+}
+function Redir_InicioAdmin() {
+
+    window.location.href = "PrincipalAdmi.aspx";
+}
+function Redir_Login() {
+
+    window.location.href = "loggin.aspx";
+}
+function Redir_FormacionA()
+{
+    window.location.href = "FormacionA.aspx"
+}
+
+
+
+
